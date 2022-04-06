c71f2c6ac4395654ce7c3618a9efa719c2e86347
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-04-06 21:04:52 +0200

    remove some NSFW content + style

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,19 +1,15 @@
+import styles from './HomePage.module.css';
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Welcome</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Welcome</h1>
       <p>
         Lorem Salu bissame ! Wie geht's les samis ? Hans apporte moi une
         Wurschtsalad avec un picon bitte, s'il te plaît. Voss ? Une Carola et du
         Melfor ? Yo dû, espèce de Knäckes, ch'ai dit un picon !
       </p>
-      <p>
-        Hopla vous savez que la mamsell Huguette, la miss Miss Dahlias du messti
-        de Bischheim était au Christkindelsmärik en compagnie de Richard
-        Schirmeck (celui qui a un blottkopf), le mari de Chulia Roberstau, qui
-        lui trempait sa Nüdle dans sa Schneck ! Yo dû, Pfourtz ! Ch'espère
-        qu'ils avaient du Kabinetpapier, Gal !
-      </p>
+
       <p>
         Yoo ch'ai lu dans les DNA que le Racing a encore perdu contre
         Oberschaeffolsheim. Verdammi et moi ch'avais donc parié deux knacks et
@@ -27,7 +23,6 @@ export default function HomePage() {
         Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ? Yeuh non, merci
         vielmols mais che dois partir à la Coopé de Truchtersheim acheter des
         mänele et des rossbolla pour les gamins. Hopla tchao bissame !
-        Consectetur adipiscing elit
       </p>
     </div>
   );
