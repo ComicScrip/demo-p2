c64338395ed790f4e1b8c91d6e193a1ccbe77be2
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-04-06 17:10:08 +0200

    member list + member card responsive

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -1,9 +1,14 @@
+import styles from './TeamMember.module.css';
+
 export default function TeamMember({ name, bio, githubLink }) {
   return (
-    <div>
-      <p>{name}</p>
-      <p>{bio}</p>
-      <p>{githubLink}</p>
+    <div className={styles.card}>
+      <p className={styles.name}>{name}</p>
+      <a href={githubLink}>
+        <img className={styles.avatar} src={`${githubLink}.png`} alt={name} />
+      </a>
+
+      <p className={styles.bio}>{bio}</p>
     </div>
   );
 }
--- src/screens/AboutPage.js
+++ src/screens/AboutPage.js
@@ -1,19 +1,22 @@
 import TeamMember from '../components/TeamMember';
+import styles from './AboutPage.module.css';
 
 export default function AboutPage() {
   return (
-    <>
-      <h1>Notre équipe</h1>
-      <TeamMember
-        name="Pierre"
-        bio={'bio Pierre'}
-        githubLink="https://github.com/NycoChavanoud"
-      />
-      <TeamMember
-        name="Pierre"
-        bio={'Bio Nico'}
-        githubLink="https://github.com/NycoChavanoud"
-      />
-    </>
+    <div>
+      <h1 className={styles.title}>Notre équipe</h1>
+      <div className={styles.memberList}>
+        <TeamMember
+          name="Nico"
+          bio={'Bio Nico'}
+          githubLink="https://github.com/NycoChavanoud"
+        />
+        <TeamMember
+          name="Pierre"
+          bio={'Bio Pierre'}
+          githubLink="https://github.com/ComicScrip"
+        />
+      </div>
+    </div>
   );
 }
